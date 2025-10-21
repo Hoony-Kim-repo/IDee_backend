625082f4b7e472b8489b7d92979e076418bd8cd4
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-10-21 15:59:14 -0400

    add middleware

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["http://localhost:5173"]
 
 app = FastAPI()
 
-@app.get('/')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
 def read_root():
     return {"message": "Hello, Backend is Working"}
